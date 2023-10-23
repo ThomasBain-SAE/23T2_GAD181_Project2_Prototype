97d4c75e81784defb88ce49197d13f46642424e5
ThomasBain-SAE <136556449+ThomasBain-SAE@users.noreply.github.com>
2023-10-23 21:51:14 +1100

    character moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 2.5f;
+    public float moveSpeed = 5.0f;
+    public float rotationSpeed = 100.0f;
+    private Rigidbody rb;
     public float sensitivity = 2f;
-
     private Transform playerTransform;
     private Transform cameraTransform;
 
     private float rotationX = 0;
-
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // Prevents the player from tilting due to physics
         playerTransform = transform;
         cameraTransform = Camera.main.transform;
         Cursor.lockState = CursorLockMode.Locked;
@@ -22,12 +25,18 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Player movement using "W," "A," "S," "D" keys
+        // Player movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+
         Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
-        Vector3 moveVelocity = moveDirection * moveSpeed;
-        playerTransform.Translate(moveVelocity * Time.deltaTime);
+
+        // Rotate the player based on the input
+        transform.Rotate(Vector3.up * horizontalInput * rotationSpeed * Time.deltaTime);
+
+        // Apply movement using forces to the Rigidbody
+        Vector3 moveVelocity = transform.forward * moveSpeed * verticalInput;
+        rb.velocity = new Vector3(moveVelocity.x, rb.velocity.y, moveVelocity.z);
 
         // Camera rotation based on mouse input
         float mouseX = Input.GetAxis("Mouse X");
